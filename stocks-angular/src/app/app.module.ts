e78707051d4e768f78e934a3b5922c8a2a785e16
diogoasp97 <diogoasp97@gmail.com>
2022-10-12 10:53:50 +0100

    Angular UI Stock List

--- stocks-angular/src/app/app.module.ts
+++ stocks-angular/src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StockListComponent } from './stock-list/stock-list.component';
 import { StockComponent } from './stock/stock.component';
 import { OpiniaoComponent } from './opiniao/opiniao.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -11,7 +10,6 @@ import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
-    StockListComponent,
     StockComponent,
     OpiniaoComponent
   ],
